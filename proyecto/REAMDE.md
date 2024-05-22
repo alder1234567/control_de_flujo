c04093349714ff96d3033a7eeaa62d2e0738c448
alder1234567 <aldergarriazo@gmail.com>
2024-05-22 10:16:11 -0500

    actualize

---
+++ proyecto/REAMDE.md
@@ -0,0 +1,8 @@
+sistema de control de gras sintetico 
+
+el usuario tiene un gras cual administrar de manera manual , al usuario necesita que se automatiza el proceso de la recerva y pago del alquiler , para lo cual solicita automatizar los siguientes casos de uso 
+
+* el usuario podra ver las listas de horario disponible 
+* el usuario podra reservar en uno de los orarios disponibles
+* el usuario podra pagar por el alquiler de la reserva realizada 
+* el usuario podria verificar su alquiler el cual le mostrara  los detalles como la fecha , hora y costo del aquiler que realizo 
---
+++ proyecto/main.py
@@ -0,0 +1,16 @@
+class Alquiler:                                                                                                                                                                                                                       
+    def __init__(self):                                                                                                                                                               
+      self.horario_disponible=
+["10:00,"12:00","15:00"]
+       self.reserva = None
+
+def 
+ver_horario_disponoble(self):
+      print("horarios disponibles:")
+      for horario in 
+self.horarios_disponibles:
+        print(horario)  
+ def reserva_alquiler(self.horario-elegido): 
+        if horario_disponibles:
+        self,reserva
+
